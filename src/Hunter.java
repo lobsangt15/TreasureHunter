aae2054bb62399322184461f59c3b5ec079f8174
Lobsang Tashi <lobsangt15@nycstudents.net>
2025-01-17 00:58:10 -0500

    completed TH-1

--- src/Hunter.java
+++ src/Hunter.java
@@ -125,12 +125,11 @@ public class Hunter {
      * @return The printable String representation of the inventory.
      */
     public String getInventory() {
-        String printableKit = "";
+        String printableKit = Colors.PURPLE +"" + Colors.RESET;
         String space = " ";
-
         for (String item : kit) {
             if (item != null) {
-                printableKit += item + space;
+                printableKit += Colors.PURPLE + item +  Colors.RESET + space;
             }
         }
         return printableKit;
@@ -140,7 +139,7 @@ public class Hunter {
      * @return A string representation of the hunter.
      */
     public String infoString() {
-        String str = hunterName + " has " + gold + " gold";
+        String str = hunterName + " has " + Colors.YELLOW + gold + " gold" + Colors.RESET;
         if (!kitIsEmpty()) {
             str += " and " + getInventory();
         }
